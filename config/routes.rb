398b272e46721a3da88c81de3d9be84237145702
kaiyu-tech <kaiyu326@gmail.com>
2021-08-20 14:26:59 +0900

    不要なコメント文を削除しました。

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ Rails.application.routes.draw do
 
   root 'books#index'
 
-  # get '/users', to: 'users#index'
-  # get '/users/:id', to: 'users#show'
-
   resources :users
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
