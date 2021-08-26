78dd569ef4a70eb27e35dc8df5d1aa05f860979e
kaiyu-tech <kaiyu326@gmail.com>
2021-08-26 10:34:23 +0900

    ルーティングの作成を制限

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   root 'books#index'
 
-  resources :users
+  resources :users, only: [:index, :show]
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
