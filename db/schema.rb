7b9fa1971d6ac2e1164c434da15634a6315314fe
kaiyu-tech <kaiyu326@gmail.com>
2021-09-01 05:57:56 +0900

    Bookテーブルにユーザーを勘違いで追加したのを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_092214) do
+ActiveRecord::Schema.define(version: 2021_08_31_205427) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,12 +43,10 @@ ActiveRecord::Schema.define(version: 2021_08_23_092214) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
-    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
     t.string "picture"
-    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -98,7 +96,6 @@ ActiveRecord::Schema.define(version: 2021_08_23_092214) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "books", "users"
   add_foreign_key "comments", "users"
   add_foreign_key "reports", "users"
 end
